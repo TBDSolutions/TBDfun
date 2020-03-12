361ffb0d671e1fe6862a4f44304ec9ed4248a7b4
Giridhar <giridhark@prodigygroupindia.com>
2020-03-12 19:22:46 +0530

    Map Function

---
+++ R/Map_function/data_retrival.R
@@ -0,0 +1,24 @@
+library(magrittr);library(tidyverse)
+library(xts);library(car)
+library(DT);library(scales);library(plotly);library(dygraphs)
+library(RColorBrewer)
+library(viridis); library(maps); library(tigris); library(leaflet)
+library(tidycensus)
+library(stringr)
+library(sf)
+
+#Download the MI geography dataset 
+county <- get_acs(geography = "county",
+                       variables = "B01003_001",
+                       state = "MI",
+                       geometry = TRUE)
+
+st_write(county, "data/county.shp")
+
+#Download the MI census tract geography dataset 
+tract <- get_acs(geography = "tract",
+                       variables = "B01003_001",
+                       state = "MI",
+                       geometry = TRUE)
+
+st_write(tract, "data/tract.shp")
