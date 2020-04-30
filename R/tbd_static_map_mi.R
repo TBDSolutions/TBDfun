665b2e0bd5f4da549ae6386732ab2b338c25a92d
venky <57141888+venky151@users.noreply.github.com>
2020-04-30 19:19:11 +0530

    Create geography dataframe for AAA regions
    Create geography dataframe for AAA regions

--- R/tbd_static_map_mi.R
+++ R/tbd_static_map_mi.R
@@ -65,91 +65,43 @@ tbd_static_map_mi <- function(df,
                         if(requireNamespace("rmapshaper"))
 
     mi_master_polygons <- st_read(system.file("extdata","mi_master_polygons.shp", package = "TBDfun"))
-    mi_master_polygons <- ms_simplify(mi_master_polygons)
 
-    ###county shape file ##
-  county <- mi_master_polygons %>%
-    group_by(county) %>%
-    summarize(
-      population = sum(estimate, na.rm = TRUE))
-  county <- ms_simplify(county)
+    col_dist <- colorBin(c(col_pallet),df$summary, reverse = T, na.color = "grey")
 
-  ###pihp shape file ##
-  pihp <- mi_master_polygons %>%
-    group_by(PIHP) %>%
-    summarize(
-      population = sum(estimate, na.rm = TRUE))
-  pihp <- ms_simplify(pihp)
+  map <- if(grepl("county", map_type, ignore.case = T)){
 
-  ###cmhsp shape file ##
-  cmhsp <- mi_master_polygons %>%
-    group_by(CMHSP) %>%
-    summarize(
-      population = sum(estimate, na.rm = TRUE))
-  cmhsp <- ms_simplify(cmhsp)
-
-  ###tract shape file ##
-  tract <- mi_master_polygons %>%
-    group_by(NAME) %>%
-    summarize(
-      population = sum(estimate, na.rm = TRUE))
-  tract <- ms_simplify(tract)
-
-  county_reference<-mi_master_polygons[,c(1,4,6)]
-  county_reference$GEOID<-substr(county_reference$GEOID, 1, 5)
-  county_reference<-county_reference %>%
-    group_by(GEOID,county)%>%
-    summarize(
-      population = sum(estimate, na.rm = TRUE))
-  county_reference$geometry<-NULL
-  county_reference$population<-NULL
-  tract_reference<-mi_master_polygons[,c(1,2)]
-  tract_reference$geometry<-NULL
-
-  if(names(df) == "countyid"){
-    df<-df %>%
-      inner_join(county_reference, by = c("countyid" = "GEOID"))%>%
-      rename(name = county)
-  }
-
-  if(names(df) == "tractid"){
-    df<-df %>%
-      inner_join(tract_reference, by = c("tractid" = "GEOID"))%>%
-      rename(name = NAME)
-  }
-
-  if(grepl("county", map_type, ignore.case = T)){
+    county_reference<-mi_master_polygons[,c(1,4,6)]
+    county_reference$GEOID<-substr(county_reference$GEOID, 1, 5)
+    county_reference<-county_reference %>%
+      group_by(GEOID,county)%>%
+      summarize(
+        population = sum(estimate, na.rm = TRUE))
+    county_reference$geometry<-NULL
+    county_reference$population<-NULL
 
     df <- county_reference %>%
       left_join(df, by = c("county" = "name")) %>%
       rename(name = county)
     df$summary <- as.numeric(df$summary)
-  }
 
-  county_label <- sprintf(
-    "<strong>%g </strong><br/><strong>%s county</strong><br/>",
-    df$summary,df$name) %>%
-    lapply(htmltools::HTML)
+    if(names(df) == "countyid"){
+      df<-df %>%
+        inner_join(county_reference, by = c("countyid" = "GEOID"))%>%
+        rename(name = county)
+    }
 
-  pihp_label <- sprintf(
-    "<strong>%g </strong><br/><strong>%s PIHP</strong><br/>",
-    df$summary,df$name) %>%
-    lapply(htmltools::HTML)
+    ###county shape file ##
+    county <- mi_master_polygons %>%
+      group_by(county) %>%
+      summarize(
+        population = sum(estimate, na.rm = TRUE))
+    county <- ms_simplify(county)
 
-  cmhsp_label <- sprintf(
-    "<strong>%g </strong><br/><strong>%s CMHSP</strong><br/>",
-    df$summary,df$name) %>%
-    lapply(htmltools::HTML)
+    county_label <- sprintf(
+      "<strong>%g </strong><br/><strong>%s county</strong><br/>",
+      df$summary,df$name) %>%
+      lapply(htmltools::HTML)
 
-  tract_label <- sprintf(
-    "<strong>%g </strong><br/><strong>%s tract</strong><br/>",
-    df$summary,df$name) %>%
-    lapply(htmltools::HTML)
-
-
-  col_dist <- colorBin(c(col_pallet),df$summary, reverse = T, na.color = "grey")
-
-  map <- if(grepl("county", map_type, ignore.case = T)){
 
     map <- leaflet(county) %>%
       addTiles() %>%
@@ -184,6 +136,18 @@ tbd_static_map_mi <- function(df,
   }
   else if(grepl("pihp", map_type, ignore.case = T)){
 
+    ###pihp shape file ##
+    pihp <- mi_master_polygons %>%
+      group_by(PIHP) %>%
+      summarize(
+        population = sum(estimate, na.rm = TRUE))
+    pihp <- ms_simplify(pihp)
+
+    pihp_label <- sprintf(
+      "<strong>%g </strong><br/><strong>%s PIHP</strong><br/>",
+      df$summary,df$name) %>%
+      lapply(htmltools::HTML)
+
     map <- leaflet(pihp) %>%
       addTiles() %>%
       addProviderTiles(addtiles) %>%
@@ -215,6 +179,18 @@ tbd_static_map_mi <- function(df,
     print(map)
   }
   else if(grepl("cmhsp", map_type, ignore.case = T)){
+
+    ###cmhsp shape file ##
+    cmhsp <- mi_master_polygons %>%
+      group_by(CMHSP) %>%
+      summarize(
+        population = sum(estimate, na.rm = TRUE))
+    cmhsp <- ms_simplify(cmhsp)
+
+    cmhsp_label <- sprintf(
+      "<strong>%g </strong><br/><strong>%s CMHSP</strong><br/>",
+      df$summary,df$name) %>%
+      lapply(htmltools::HTML)
 
     map <- leaflet(cmhsp) %>%
       addTiles() %>%
@@ -248,6 +224,25 @@ tbd_static_map_mi <- function(df,
   }
   else if(grepl("tract", map_type, ignore.case = T)){
 
+    tract_reference<-mi_master_polygons[,c(1,2)]
+    tract_reference$geometry<-NULL
+
+    if(names(df) == "tractid"){
+      df<-df %>%
+        inner_join(tract_reference, by = c("tractid" = "GEOID"))%>%
+        rename(name = NAME)
+    }
+    ###tract shape file ##
+    tract <- mi_master_polygons %>%
+      group_by(NAME) %>%
+      summarize(
+        population = sum(estimate, na.rm = TRUE))
+
+    tract_label <- sprintf(
+      "<strong>%g </strong><br/><strong>%s tract</strong><br/>",
+      df$summary,df$name) %>%
+      lapply(htmltools::HTML)
+
     map <- leaflet(tract) %>%
       addTiles() %>%
       addProviderTiles(addtiles) %>%
@@ -265,6 +260,51 @@ tbd_static_map_mi <- function(df,
           fillOpacity = 0.7,
           bringToFront = F),
         label = tract_label,
+        labelOptions = labelOptions(
+          style = list("font-weight" = "normal", padding = "3px 8px"),
+          textsize = "15px",
+          direction = "auto")) %>%
+      leaflet::addLegend(pal = col_dist,
+                         values = ~df$summary,
+                         opacity = 0.7,
+                         title = legend_label,
+                         position = "bottomright",
+                         na.label = "NA")
+
+    print(map)
+  }
+
+  else if(grepl("agency", map_type, ignore.case = T)){
+
+    ###agency shape file ##
+    agency <- mi_master_polygons %>%
+      group_by(agency) %>%
+      summarize(
+        population = sum(estimate, na.rm = TRUE))
+    agency <- ms_simplify(agency)
+
+    agency_label <- sprintf(
+      "<strong>%g </strong><br/><strong>%s agency</strong><br/>",
+      df$summary,df$name) %>%
+      lapply(htmltools::HTML)
+
+    map <- leaflet(agency) %>%
+      addTiles() %>%
+      addProviderTiles(addtiles) %>%
+      addPolygons(
+        fillColor = ~col_dist(df$summary),
+        weight = 2,
+        opacity = 0.6,
+        color = border_col,
+        dashArray = "3",
+        fillOpacity = 0.75,
+        highlight = highlightOptions(
+          weight = 5,
+          color = "#FF5500",
+          dashArray = "0.3",
+          fillOpacity = 0.7,
+          bringToFront = F),
+        label = agency_label,
         labelOptions = labelOptions(
           style = list("font-weight" = "normal", padding = "3px 8px"),
           textsize = "15px",
--- tools/data_retrieval.R
+++ tools/data_retrieval.R
@@ -6,85 +6,134 @@ library(viridis); library(maps); library(tigris); library(leaflet)
 library(tidycensus)
 library(stringr)
 library(sf)
+library(rmapshaper)
 
 #Download the MI geography dataset
-mi_polygons <- get_acs(geography = "tract",
+mi_master_polygons <- get_acs(geography = "tract",
                        variables = "B01003_001",
                        state = "MI",
                        geometry = TRUE)
 
-mi_polygons$county<-str_extract(mi_polygons$NAME, "(?<=,)[^,]*(?=,)")
-mi_polygons$county<-gsub(" County","",mi_polygons$county)
-mi_polygons$county<-trimws(mi_polygons$county)
-mi_polygons$NAME<-gsub("St. Clair","Saint Clair",mi_polygons$NAME)
-mi_polygons$NAME<-gsub("St. Joseph","Saint Joseph",mi_polygons$NAME)
-mi_polygons$county<-gsub("St. Clair","Saint Clair",mi_polygons$county)
-mi_polygons$county<-gsub("St. Joseph","Saint Joseph",mi_polygons$county)
+mi_master_polygons$county<-str_extract(mi_master_polygons$NAME, "(?<=,)[^,]*(?=,)")
+mi_master_polygons$county<-gsub(" County","",mi_master_polygons$county)
+mi_master_polygons$county<-trimws(mi_master_polygons$county)
+mi_master_polygons$NAME<-gsub("St. Clair","Saint Clair",mi_master_polygons$NAME)
+mi_master_polygons$NAME<-gsub("St. Joseph","Saint Joseph",mi_master_polygons$NAME)
+mi_master_polygons$county<-gsub("St. Clair","Saint Clair",mi_master_polygons$county)
+mi_master_polygons$county<-gsub("St. Joseph","Saint Joseph",mi_master_polygons$county)
 #######
-mi_polygons$PIHP<-NA
-mi_polygons[mi_polygons$county %in% c("Lenawee","Livingston","Monroe","Washtenaw"),]$PIHP <- 'CMHPSM'
-mi_polygons[mi_polygons$county %in% c("Wayne"),]$PIHP <- 'DWMHA'
-mi_polygons[mi_polygons$county %in% c("Oakland"),]$PIHP <- 'OCCMHA'
-mi_polygons[mi_polygons$county %in% c("Allegan", "Kent","Lake", "Mason","Muskegon", "Oceana","Ottawa"),]$PIHP <- 'LRE'
-mi_polygons[mi_polygons$county %in% c("Macomb"),]$PIHP <- 'MCMHS'
-mi_polygons[mi_polygons$county %in% c("Arenac","Bay", "Clare","Clinton","Eaton","Gladwin", "Gratiot","Hillsdale",
+mi_master_polygons$PIHP<-NA
+mi_master_polygons[mi_master_polygons$county %in% c("Lenawee","Livingston","Monroe","Washtenaw"),]$PIHP <- 'CMHPSM'
+mi_master_polygons[mi_master_polygons$county %in% c("Wayne"),]$PIHP <- 'DWMHA'
+mi_master_polygons[mi_master_polygons$county %in% c("Oakland"),]$PIHP <- 'OCCMHA'
+mi_master_polygons[mi_master_polygons$county %in% c("Allegan", "Kent","Lake", "Mason","Muskegon", "Oceana","Ottawa"),]$PIHP <- 'LRE'
+mi_master_polygons[mi_master_polygons$county %in% c("Macomb"),]$PIHP <- 'MCMHS'
+mi_master_polygons[mi_master_polygons$county %in% c("Arenac","Bay", "Clare","Clinton","Eaton","Gladwin", "Gratiot","Hillsdale",
                                       "Huron","Ingham", "Ionia","Isabella","Jackson","Mecosta", "Midland","Montcalm",
                                       "Newaygo","Osceola","Saginaw","Shiawassee","Tuscola"),]$PIHP <- 'MSHN'
-mi_polygons[mi_polygons$county %in% c("Alcona","Alpena", "Antrim","Benzie", "Charlevoix","Cheboygan","Crawford","Emmet",
+mi_master_polygons[mi_master_polygons$county %in% c("Alcona","Alpena", "Antrim","Benzie", "Charlevoix","Cheboygan","Crawford","Emmet",
                                       "Grand Traverse","Iosco","Kalkaska","Leelanau","Manistee","Missaukee","Montmorency","Ogemaw",
                                       "Oscoda","Otsego","Presque Isle","Roscommon","Wexford"),]$PIHP <- 'NMRE'
-mi_polygons[mi_polygons$county %in% c("Alger","Baraga","Chippewa","Delta", "Dickinson","Gogebic","Houghton","Iron",
+mi_master_polygons[mi_master_polygons$county %in% c("Alger","Baraga","Chippewa","Delta", "Dickinson","Gogebic","Houghton","Iron",
                                       "Keweenaw","Luce","Mackinac","Marquette","Menominee","Ontonagon","Schoolcraft"),]$PIHP <- 'Northcare'
-mi_polygons[mi_polygons$county %in% c("Genesee","Lapeer","Sanilac","Saint Clair"),]$PIHP <- 'Region10'
-mi_polygons[mi_polygons$county %in% c("Barry","Berrien","Branch","Calhoun","Cass","Kalamazoo","Van Buren","Saint Joseph"),]$PIHP <- 'SWMBH'
+mi_master_polygons[mi_master_polygons$county %in% c("Genesee","Lapeer","Sanilac","Saint Clair"),]$PIHP <- 'Region10'
+mi_master_polygons[mi_master_polygons$county %in% c("Barry","Berrien","Branch","Calhoun","Cass","Kalamazoo","Van Buren","Saint Joseph"),]$PIHP <- 'SWMBH'
 
-mi_polygons$CMHSP<-NA
-mi_polygons[mi_polygons$county %in% c("Allegan"),]$CMHSP <- 'Allegan'
-mi_polygons[mi_polygons$county %in% c("Iosco","Ogemaw","Oscoda"),]$CMHSP <- 'AuSable Valley'
-mi_polygons[mi_polygons$county %in% c("Barry"),]$CMHSP <- 'Barry'
-mi_polygons[mi_polygons$county %in% c("Oakland"),]$CMHSP <- 'Oakland'
-mi_polygons[mi_polygons$county %in% c("Saint Clair"),]$CMHSP <- 'Saint Clair'
-mi_polygons[mi_polygons$county %in% c("Saint Joseph"),]$CMHSP <- 'Saint Joseph'
-mi_polygons[mi_polygons$county %in% c("Arenac","Bay"),]$CMHSP <- 'Bay-Arenac'
-mi_polygons[mi_polygons$county %in% c("Berrien"),]$CMHSP <- 'Berrien'
-mi_polygons[mi_polygons$county %in% c("Clinton","Eaton","Ingham"),]$CMHSP <- 'CEI'
-mi_polygons[mi_polygons$county %in% c("Clare","Gladwin", "Isabella","Mecosta","Midland","Osceola"),]$CMHSP <- 'Central Michigan'
-mi_polygons[mi_polygons$county %in% c("Baraga","Houghton","Keweenaw","Ontonagon"),]$CMHSP <- 'Copper Country'
-mi_polygons[mi_polygons$county %in% c("Wayne"),]$CMHSP <- 'Detroit-Wayne'
-mi_polygons[mi_polygons$county %in% c("Genesee"),]$CMHSP <- 'Genesee'
-mi_polygons[mi_polygons$county %in% c("Gogebic"),]$CMHSP <- 'Gogebic'
-mi_polygons[mi_polygons$county %in% c("Gratiot"),]$CMHSP <- 'Gratiot'
-mi_polygons[mi_polygons$county %in% c("Chippewa","Mackinac","Schoolcraft"),]$CMHSP <- 'Hiawatha'
-mi_polygons[mi_polygons$county %in% c("Huron"),]$CMHSP <- 'Huron'
-mi_polygons[mi_polygons$county %in% c("Ionia"),]$CMHSP <- 'Ionia'
-mi_polygons[mi_polygons$county %in% c("Kalamazoo"),]$CMHSP <- 'Kalamazoo'
-mi_polygons[mi_polygons$county %in% c("Lapeer"),]$CMHSP <- 'Lapeer'
-mi_polygons[mi_polygons$county %in% c("Lenawee"),]$CMHSP <- 'Lenawee'
-mi_polygons[mi_polygons$county %in% c("Hillsdale","Jackson"),]$CMHSP <- 'Lifeways'
-mi_polygons[mi_polygons$county %in% c("Livingston"),]$CMHSP <- 'Livingston'
-mi_polygons[mi_polygons$county %in% c("Macomb"),]$CMHSP <- 'Macomb'
-mi_polygons[mi_polygons$county %in% c("Benzie","Manistee"),]$CMHSP <- 'Manistee-Benzie'
-mi_polygons[mi_polygons$county %in% c("Monroe"),]$CMHSP <- 'Monroe'
-mi_polygons[mi_polygons$county %in% c("Montcalm"),]$CMHSP <- 'Montcalm'
-mi_polygons[mi_polygons$county %in% c("Muskegon"),]$CMHSP <- 'Muskegon'
-mi_polygons[mi_polygons$county %in% c("Kent"),]$CMHSP <- 'Network180'
-mi_polygons[mi_polygons$county %in% c("Newaygo"),]$CMHSP <- 'Newaygo'
-mi_polygons[mi_polygons$county %in% c("Antrim","Charlevoix","Cheboygan","Emmet","Kalkaska","Otsego"),]$CMHSP <- 'North Country'
-mi_polygons[mi_polygons$county %in% c("Alcona","Alpena","Montmorency","Presque Isle"),]$CMHSP <- 'Northeast Michigan'
-mi_polygons[mi_polygons$county %in% c("Crawford","Grand Traverse","Leelanau", "Missaukee","Roscommon","Wexford"),]$CMHSP <- 'Northern Lakes'
-mi_polygons[mi_polygons$county %in% c("Dickinson","Iron","Menominee"),]$CMHSP <- 'Northpointe'
-mi_polygons[mi_polygons$county %in% c("Ottawa"),]$CMHSP <- 'Ottawa'
-mi_polygons[mi_polygons$county %in% c("Alger","Delta","Luce","Marquette"),]$CMHSP <- 'Pathways'
-mi_polygons[mi_polygons$county %in% c("Branch"),]$CMHSP <- 'Pines'
-mi_polygons[mi_polygons$county %in% c("Saginaw"),]$CMHSP <- 'Saginaw'
-mi_polygons[mi_polygons$county %in% c("Sanilac"),]$CMHSP <- 'Sanilac'
-mi_polygons[mi_polygons$county %in% c("Shiawassee"),]$CMHSP <- 'Shiawassee'
-mi_polygons[mi_polygons$county %in% c("Calhoun"),]$CMHSP <- 'Summit Pointe'
-mi_polygons[mi_polygons$county %in% c("Tuscola"),]$CMHSP <- 'Tuscola'
-mi_polygons[mi_polygons$county %in% c("Van Buren"),]$CMHSP <- 'Van Buren'
-mi_polygons[mi_polygons$county %in% c("Cass"),]$CMHSP <- 'Woodlands'
-mi_polygons[mi_polygons$county %in% c("Lake","Mason","Oceana"),]$CMHSP <- 'West Michigan'
-mi_polygons[mi_polygons$county %in% c("Washtenaw"),]$CMHSP <- 'Washtenaw'
+mi_master_polygons$CMHSP<-NA
+mi_master_polygons[mi_master_polygons$county %in% c("Allegan"),]$CMHSP <- 'Allegan'
+mi_master_polygons[mi_master_polygons$county %in% c("Iosco","Ogemaw","Oscoda"),]$CMHSP <- 'AuSable Valley'
+mi_master_polygons[mi_master_polygons$county %in% c("Barry"),]$CMHSP <- 'Barry'
+mi_master_polygons[mi_master_polygons$county %in% c("Oakland"),]$CMHSP <- 'Oakland'
+mi_master_polygons[mi_master_polygons$county %in% c("Saint Clair"),]$CMHSP <- 'Saint Clair'
+mi_master_polygons[mi_master_polygons$county %in% c("Saint Joseph"),]$CMHSP <- 'Saint Joseph'
+mi_master_polygons[mi_master_polygons$county %in% c("Arenac","Bay"),]$CMHSP <- 'Bay-Arenac'
+mi_master_polygons[mi_master_polygons$county %in% c("Berrien"),]$CMHSP <- 'Berrien'
+mi_master_polygons[mi_master_polygons$county %in% c("Clinton","Eaton","Ingham"),]$CMHSP <- 'CEI'
+mi_master_polygons[mi_master_polygons$county %in% c("Clare","Gladwin", "Isabella","Mecosta","Midland","Osceola"),]$CMHSP <- 'Central Michigan'
+mi_master_polygons[mi_master_polygons$county %in% c("Baraga","Houghton","Keweenaw","Ontonagon"),]$CMHSP <- 'Copper Country'
+mi_master_polygons[mi_master_polygons$county %in% c("Wayne"),]$CMHSP <- 'Detroit-Wayne'
+mi_master_polygons[mi_master_polygons$county %in% c("Genesee"),]$CMHSP <- 'Genesee'
+mi_master_polygons[mi_master_polygons$county %in% c("Gogebic"),]$CMHSP <- 'Gogebic'
+mi_master_polygons[mi_master_polygons$county %in% c("Gratiot"),]$CMHSP <- 'Gratiot'
+mi_master_polygons[mi_master_polygons$county %in% c("Chippewa","Mackinac","Schoolcraft"),]$CMHSP <- 'Hiawatha'
+mi_master_polygons[mi_master_polygons$county %in% c("Huron"),]$CMHSP <- 'Huron'
+mi_master_polygons[mi_master_polygons$county %in% c("Ionia"),]$CMHSP <- 'Ionia'
+mi_master_polygons[mi_master_polygons$county %in% c("Kalamazoo"),]$CMHSP <- 'Kalamazoo'
+mi_master_polygons[mi_master_polygons$county %in% c("Lapeer"),]$CMHSP <- 'Lapeer'
+mi_master_polygons[mi_master_polygons$county %in% c("Lenawee"),]$CMHSP <- 'Lenawee'
+mi_master_polygons[mi_master_polygons$county %in% c("Hillsdale","Jackson"),]$CMHSP <- 'Lifeways'
+mi_master_polygons[mi_master_polygons$county %in% c("Livingston"),]$CMHSP <- 'Livingston'
+mi_master_polygons[mi_master_polygons$county %in% c("Macomb"),]$CMHSP <- 'Macomb'
+mi_master_polygons[mi_master_polygons$county %in% c("Benzie","Manistee"),]$CMHSP <- 'Manistee-Benzie'
+mi_master_polygons[mi_master_polygons$county %in% c("Monroe"),]$CMHSP <- 'Monroe'
+mi_master_polygons[mi_master_polygons$county %in% c("Montcalm"),]$CMHSP <- 'Montcalm'
+mi_master_polygons[mi_master_polygons$county %in% c("Muskegon"),]$CMHSP <- 'Muskegon'
+mi_master_polygons[mi_master_polygons$county %in% c("Kent"),]$CMHSP <- 'Network180'
+mi_master_polygons[mi_master_polygons$county %in% c("Newaygo"),]$CMHSP <- 'Newaygo'
+mi_master_polygons[mi_master_polygons$county %in% c("Antrim","Charlevoix","Cheboygan","Emmet","Kalkaska","Otsego"),]$CMHSP <- 'North Country'
+mi_master_polygons[mi_master_polygons$county %in% c("Alcona","Alpena","Montmorency","Presque Isle"),]$CMHSP <- 'Northeast Michigan'
+mi_master_polygons[mi_master_polygons$county %in% c("Crawford","Grand Traverse","Leelanau", "Missaukee","Roscommon","Wexford"),]$CMHSP <- 'Northern Lakes'
+mi_master_polygons[mi_master_polygons$county %in% c("Dickinson","Iron","Menominee"),]$CMHSP <- 'Northpointe'
+mi_master_polygons[mi_master_polygons$county %in% c("Ottawa"),]$CMHSP <- 'Ottawa'
+mi_master_polygons[mi_master_polygons$county %in% c("Alger","Delta","Luce","Marquette"),]$CMHSP <- 'Pathways'
+mi_master_polygons[mi_master_polygons$county %in% c("Branch"),]$CMHSP <- 'Pines'
+mi_master_polygons[mi_master_polygons$county %in% c("Saginaw"),]$CMHSP <- 'Saginaw'
+mi_master_polygons[mi_master_polygons$county %in% c("Sanilac"),]$CMHSP <- 'Sanilac'
+mi_master_polygons[mi_master_polygons$county %in% c("Shiawassee"),]$CMHSP <- 'Shiawassee'
+mi_master_polygons[mi_master_polygons$county %in% c("Calhoun"),]$CMHSP <- 'Summit Pointe'
+mi_master_polygons[mi_master_polygons$county %in% c("Tuscola"),]$CMHSP <- 'Tuscola'
+mi_master_polygons[mi_master_polygons$county %in% c("Van Buren"),]$CMHSP <- 'Van Buren'
+mi_master_polygons[mi_master_polygons$county %in% c("Cass"),]$CMHSP <- 'Woodlands'
+mi_master_polygons[mi_master_polygons$county %in% c("Lake","Mason","Oceana"),]$CMHSP <- 'West Michigan'
+mi_master_polygons[mi_master_polygons$county %in% c("Washtenaw"),]$CMHSP <- 'Washtenaw'
 
-st_write(mi_polygons,"data/mi_master_polygons.shp")
+mi_master_polygons$agency<-NA
+mi_master_polygons[mi_master_polygons$county %in% c("Wayne"),]$agency <- 'Region 1A & 1C'
+mi_master_polygons[mi_master_polygons$county %in% c("Livingston","Macomb","Monroe","Oakland","Saint Clair","Washtenaw"),]$agency <- 'Region 1B'
+mi_master_polygons[mi_master_polygons$county %in% c("Jackson", "Hillsdale", "Lenawee"),]$agency <- 'Region 2'
+mi_master_polygons[mi_master_polygons$county %in% c("Barry", "Branch", "Calhoun", "Kalamazoo", "Saint Joseph"),]$agency <- 'Region 3'
+mi_master_polygons[mi_master_polygons$county %in% c("Berrien", "Cass", "Van Buren"),]$agency <- 'Region 4'
+mi_master_polygons[mi_master_polygons$county %in% c("Genesee", "Lapeer", "Shiawassee"),]$agency <- 'Region 5'
+mi_master_polygons[mi_master_polygons$county %in% c("Clinton", "Eaton", "Ingham"),]$agency <- 'Region 6'
+mi_master_polygons[mi_master_polygons$county %in% c("Bay", "Clare", "Gladwin", "Gratiot", "Huron", "Isabella", "Midland", "Saginaw", "Sanilac", "Tuscola"),]$agency <- 'Region 7'
+mi_master_polygons[mi_master_polygons$county %in% c("Allegan", "Ionia", "Kent", "Lake", "Mason", "Mecosta", "Montcalm", "Newaygo", "Osceola"),]$agency <- 'Region 8'
+mi_master_polygons[mi_master_polygons$county %in% c("Alcona", "Arenac", "Alpena", "Cheboygan", "Crawford", "Iosco", "Montmorency", "Ogemaw", "Oscoda", "Otsego", "Presque Isle", "Roscommon"),]$agency <- 'Region 9'
+mi_master_polygons[mi_master_polygons$county %in% c("Antrim", "Benzie", "Charlevoix", "Emmet", "Grand Traverse", "Kalkaska", "Leelanau", "Manistee", "Missaukee", "Wexford" ),]$agency <- 'Region 10'
+mi_master_polygons[mi_master_polygons$county %in% c("Alger", "Baraga", "Chippewa", "Delta", "Dickinson", "Gogebic", "Houghton", "Iron", "Keweenaw", "Luce", "Mackinac", "Marquette", "Menominee", "Ontonagon", "Schoolcraft"),]$agency <- 'Region 11'
+mi_master_polygons[mi_master_polygons$county %in% c("Muskegon", "Oceana", "Ottawa"),]$agency <- 'Region 14'
 
+###county shape file ##
+county <- mi_master_polygons %>%
+  group_by(county) %>%
+  summarize(
+    population = sum(estimate, na.rm = TRUE))
+county <- ms_simplify(county)
+
+###pihp shape file ##
+pihp <- mi_master_polygons %>%
+  group_by(PIHP) %>%
+  summarize(
+    population = sum(estimate, na.rm = TRUE))
+pihp <- ms_simplify(pihp)
+
+###cmhsp shape file ##
+cmhsp <- mi_master_polygons %>%
+  group_by(CMHSP) %>%
+  summarize(
+    population = sum(estimate, na.rm = TRUE))
+cmhsp <- ms_simplify(cmhsp)
+
+###tract shape file ##
+tract <- mi_master_polygons %>%
+  group_by(NAME) %>%
+  summarize(
+    population = sum(estimate, na.rm = TRUE))
+
+###agency shape file ##
+agency <- mi_master_polygons %>%
+  group_by(agency) %>%
+  summarize(
+    population = sum(estimate, na.rm = TRUE))
+agency <- ms_simplify(agency)
+
+st_write(mi_master_polygons,"../mi_master_polygons.shp")
