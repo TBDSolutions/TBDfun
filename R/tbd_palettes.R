837e5cb7fc93cc5fee11d2938c1ff6f41a0feaed
claudsvred <claudiav@tbdsolutions.com>
2024-04-09 16:45:08 -0500

    Update tbd_palettes.R

--- R/tbd_palettes.R
+++ R/tbd_palettes.R
@@ -1,8 +1,19 @@
 
-# large categorical variables
+# categorical variables
+pal_9 <- c("#000000","#E69F00","#56B4E9","#009E73",
+           "#F0E442","#0072B2","#D55E00","#CC79A7",
+           "#999999" )
+
+# large categorical variables (only if really necessary)
+pal_12 <- c("#88CCEE", "#CC6677", "#DDCC77", "#117733", "#332288", "#AA4499",
+            "#44AA99", "#999933", "#882255", "#661100", "#6699CC", "#888888")
 
 # continuous variables
+pal_contYB <- c('#f7feae','#b7e6a5','#7ccba2','#46aea0','#089099','#00718b','#045275')
+pal_contSunset <- c('#fcde9c','#faa476','#f0746e','#e34f6f','#dc3977','#b9257a','#7c1d6f')
 
 # diverging
+pal_div <- c('#008080','#70a494','#b4c8a8','#f6edbd','#edbb8a','#de8a5a','#ca562c')
 
 # color-blind version of red-yellow-green
+pal_ryg <- c("#D55E00","#F0E442","#009E73")
